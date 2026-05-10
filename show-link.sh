93ec0d9623121cda2b1f5a43d5782f54a21c3ee7
sajadma683-glitch <sajadma683@gmail.com>
2026-05-10 16:41:26 +0330

    Update show-link.sh

--- show-link.sh
+++ show-link.sh
@@ -2,8 +2,11 @@
 CONFIG="/etc/xray/g2ray.json"
 UUID=$(grep -o '"id": *"[^"]*"' "$CONFIG" | head -1 | grep -o '"[^"]*"$' | tr -d '"')
 if [ -z "$UUID" ]; then echo "[g2ray] UUID پیدا نشد."; exit 1; fi
+
 SNI="${CODESPACE_NAME}-443.app.github.dev"
-LINK="vless://${UUID}@94.130.50.12:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#pixel-net-6e956d"
+
+LINK="vless://${UUID}@${SNI}:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=0&type=xhttp&mode=packet-up&path=%2F#pixel-net-6e956d"
+
 echo ""
 echo "================================================"
 echo "  $LINK"
